e31f90ee40408a7eae33279b16a0b07bc6409c44
thaitrongtin <thaitrongtindev@gmail.com>
2024-03-05 16:05:52 +0700

    swtich screens

--- app/src/main/java/com/example/foodorderingapp/LoginActivity.kt
+++ app/src/main/java/com/example/foodorderingapp/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.example.foodorderingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.foodorderingapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.tvDontHaveAccount.setOnClickListener() {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/foodorderingapp/StartActivity.kt
+++ app/src/main/java/com/example/foodorderingapp/StartActivity.kt
@@ -1,11 +1,20 @@
 package com.example.foodorderingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.foodorderingapp.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_start)
+
+        binding.btn.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
