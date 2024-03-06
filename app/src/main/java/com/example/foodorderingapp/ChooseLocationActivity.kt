ac125aab482b0440b135108698866c2efcfef359
thaitrongtin <thaitrongtindev@gmail.com>
2024-03-06 23:13:27 +0700

    navigation commpoent

---
+++ app/src/main/java/com/example/foodorderingapp/ChooseLocationActivity.kt
@@ -0,0 +1,26 @@
+package com.example.foodorderingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import com.example.foodorderingapp.databinding.ActivityChooseLocationBinding
+
+class ChooseLocationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChooseLocationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_choose_location)
+
+        val locationList = arrayOf("TPHCM", "HN", "DN", "BT")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, locationList)
+
+        binding.listOfLocation.setAdapter(adapter)
+
+
+
+    }
+}
--- app/src/main/java/com/example/foodorderingapp/MainActivity.kt
+++ app/src/main/java/com/example/foodorderingapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.foodorderingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavView.setupWithNavController(navController)
     }
 }
