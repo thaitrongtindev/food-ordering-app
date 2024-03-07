5b4da6f1676ca9c0769119423cb709154d29f8ed
thaitrongtin <thaitrongtindev@gmail.com>
2024-03-07 16:07:01 +0700

    code recycerview adapter display data

--- app/src/main/java/com/example/foodorderingapp/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/foodorderingapp/Fragment/HomeFragment.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.foodorderingapp.R
+import com.example.foodorderingapp.adapter.PopularAdapter
 import com.example.foodorderingapp.databinding.FragmentHomeBinding
 
 
@@ -38,6 +41,28 @@ class HomeFragment : Fragment() {
         val imageSlider = binding.imageSlider
         imageSlider.setImageList(imageList)
         imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+
+            }
+
+        })
+
+        val foodName = listOf("Burger", "Sandwich", "Mono", "Item")
+        val price = listOf("$5", "$7", "$4", "$10")
+        val popularFoodImages = listOf(R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4)
+
+        val adapterPopular = PopularAdapter(foodName, price, popularFoodImages)
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = adapterPopular
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/foodorderingapp/adapter/PopularAdapter.kt
+++ app/src/main/java/com/example/foodorderingapp/adapter/PopularAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.foodorderingapp.adapter
 
-class PopularAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodorderingapp.R
+import com.example.foodorderingapp.databinding.PopularItemBinding
+
+class PopularAdapter(
+    private val items: List<String>,
+    private val price: List<String>,
+    private val image: List<Int>
+) : RecyclerView.Adapter<PopularAdapter.PopularViewHolder>() {
+
+    class PopularViewHolder(private val binding: PopularItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: String, price: String, image: Int) {
+            binding.tvFoodNamePopular.text = item
+            binding.tvPricePopular.text = price
+            binding.imageView.setImageResource(image)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+        val binding = PopularItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PopularViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+        val item = items[position]
+        val price = price[position]
+        val image = image[position]
+
+        holder.bind(item, price, image)
+    }
 }
